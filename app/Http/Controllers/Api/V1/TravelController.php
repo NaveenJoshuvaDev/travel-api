be5657479792f5cf14ef8d84a8b1d9458c5611ef
NaveenJoshuvaDev <njworkholic@gmail.com>
2024-08-16 09:37:15 +0530

    creating API v3

--- app/Http/Controllers/Api/V1/TravelController.php
+++ app/Http/Controllers/Api/V1/TravelController.php
@@ -4,8 +4,14 @@ namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Travel;
 
 class TravelController extends Controller
 {
     //
+
+    public function index()
+    {
+        return Travel::where('is_public', true)->get();
+    }
 }
